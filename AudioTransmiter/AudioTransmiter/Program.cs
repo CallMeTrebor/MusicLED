40284389608167d61fde71b1f2826607c3816d46
CallMeTrebor <60898829+CallMeTrebor@users.noreply.github.com>
2023-09-14 15:40:14 +0300

    Added audio tranmitter and testing receiver code

--- AudioTransmiter/AudioTransmiter/Program.cs
+++ AudioTransmiter/AudioTransmiter/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,21 @@ namespace AudioTransmiter
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            const int serverPort = 3000;
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint broadcastEndPoint = new IPEndPoint(IPAddress.Broadcast, serverPort);
+
+            WaveInEvent waveIn = new WaveInEvent() { WaveFormat = new WaveFormat(48000, 16, 1) };
+            waveIn.DataAvailable += (sender, e) =>
+            {
+                byte[] audioData = e.Buffer;
+                udpClient.Send(audioData, audioData.Length, broadcastEndPoint);
+            };
+            waveIn.StartRecording();
+
+            while (true) ;
         }
     }
 }
---
+++ AudioTransmiter/AudioTransmiterTester/Program.cs
@@ -0,0 +1,56 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioTransmiterTester
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const int listenPort = 3000; // Port to listen for UDP audio data
+
+            // Initialize NAudio's WaveOutEvent to play audio
+            WaveOutEvent waveOut = new WaveOutEvent();
+
+            try
+            {
+                // Create a UDP listener to receive audio data
+                UdpClient udpListener = new UdpClient(listenPort);
+                IPEndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, listenPort);
+
+                Console.WriteLine($"Listening for audio data on port {listenPort}");
+
+                // Initialize NAudio's WaveFormat to match the audio format you expect
+                var waveFormat = new WaveFormat(48000, 16, 1); // Adjust sample rate, bit depth, and channels as needed
+
+                // Create a BufferedWaveProvider for real-time audio playback
+                BufferedWaveProvider waveProvider = new BufferedWaveProvider(waveFormat);
+                waveOut.Init(waveProvider);
+                waveOut.Play();
+
+                while (true)
+                {
+                    byte[] audioData = udpListener.Receive(ref senderEndPoint);
+
+                    // Write the received audio data to the playback buffer
+                    waveProvider.AddSamples(audioData, 0, audioData.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                // Dispose of resources when done
+                waveOut.Dispose();
+            }
+        }
+    }
+}
